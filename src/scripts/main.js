1fb460a934d7db4159bb665907b93b3c9c9904a6
Kyrie Williams <wkyrie98@gmail.com>
2024-09-18 14:12:43 -0500

    Pushing fired

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,28 @@
 // Imports go first
-
-
+import { makePottery } from './potteryWheel.js'
+import { firePottery } from './kiln.js'
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+const mug = makePottery("mug", 1, 4)
+const bowl = makePottery("bowl", 5, 6)
+const teapot = makePottery("teapot", 3, 6)
+const jar = makePottery("jar", 5, 12)
+const platter = makePottery("platter", 4, 2)
+
 
 
 // Fire each piece of pottery in the kiln
+const firedMug = firePottery(mug, 1200)
+const firedBowl = firePottery(bowl, 2400)
+const firedTeapot = firePottery(teapot, 2200)
+const firedJar = firePottery(jar, 1600)
+const firedPlatter = firePottery(platter, 1000)
 
+console.log('Fired Mug', firedMug)
+console.log('Fired Bowl', firedBowl)
+console.log('FIred Teapot', firedTeapot)
+console.log('Fired Jar', firedJar)
+console.log('Fired Platter', firedPlatter)
 
 // Determine which ones should be sold, and their price
 
